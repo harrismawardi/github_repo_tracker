4a062a99ea10a41258c006090b1d36daedb94f6e
harrismawardi <harris.mawardi@yahoo.co.uk>
2021-09-14 16:58:49 +0100

    fixed export bug

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,3 @@
 export { default as BackButton } from "./BackButton";
 export { default as Form } from './Form';
-export { default as RepoCard } './RepoCard';
+export { default as RepoCard } from './RepoCard';
