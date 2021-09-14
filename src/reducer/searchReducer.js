6e09d707fc836023d0e69f9a36963d3f95f0b8b5
harrismawardi <harris.mawardi@yahoo.co.uk>
2021-09-14 15:37:06 +0100

    created switch in reducer

--- src/reducer/searchReducer.js
+++ src/reducer/searchReducer.js
@@ -1,13 +1,16 @@
 const initState = 
 {
     user: { name: '', username:'', bio: ''},
-    repos: []
+    repos: [],
+    error: false
 }
 
 const searchReducer = (state=initState, action) => {
     switch(action.type){
         case 'LOAD_RESULT': 
-            return {...action.body}
+            return {...state, repos: action.payload}
+        case 'SET_ERROR':
+            return {...state, error: action.payload}
         default:
             return state
     }
