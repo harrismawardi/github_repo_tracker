6ece1302b8f015253569ec8bfb978de8219446aa
“ELOBO5” <elicialobo@hotmail.com>
2021-09-14 21:27:02 +0100

    add test for Header

---
+++ src/layout/Header/Header.test.js
@@ -0,0 +1,12 @@
+import { render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import Header from ".";
+
+describe("Header", () => {
+  test("it renders a header", () => {
+    render(<Header />);
+    expect(screen.getByRole("heading").textContent).toBe(
+      "Enter the GitHub username to get information about your public repos."
+    );
+  });
+});
