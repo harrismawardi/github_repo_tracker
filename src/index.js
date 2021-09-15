154f85052191532c930ad3f4757bc65722ddca44
harrismawardi <harris.mawardi@yahoo.co.uk>
2021-09-15 10:44:08 +0100

    uses redux-persist

--- src/index.js
+++ src/index.js
@@ -2,15 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { BrowserRouter as Router } from "react-router-dom";
-import store from "./store";
+import { PersistGate } from "redux-persist/integration/react";
+
+import {store, persistor} from "./store";
 import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
       <Router>
         <App />
       </Router>
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
--- src/pages/Repo/index.js
+++ src/pages/Repo/index.js
@@ -1,14 +1,16 @@
 import React from "react";
 import { BackButton, RepoCard, Profile } from "../../components";
 
+import './style.css'
+
 const Repo = () => {
 
   return (
-    <>
-        <BackButton />
+    <div id='container'>
         <Profile />
         <RepoCard />
-    </>
+        <BackButton />
+    </div>
   );
 };
 
